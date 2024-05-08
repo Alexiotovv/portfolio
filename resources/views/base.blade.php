a392df56c93070a022d038cb1348c2615e718a2a
Alexiotovv <gpsalexvasquez@gmail.com>
2024-05-08 14:24:22 -0500

    fix_routes

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Alex Vasquez</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../css/bootstrap.css">
 </head>
 <body>
     
@@ -37,6 +37,6 @@
 
       @yield('content')
 
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../../../js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
